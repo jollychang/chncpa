f5705e21ca19b0e3d3b49b34fa8dc8fc7f604bdf
jollychang <jollychang@gmail.com>
2013-06-19 10:53:19 +0800

    use Notification Center

--- check_tickets.py
+++ check_tickets.py
@@ -2,7 +2,9 @@
 #! /usr/bin/python
 import urllib2
 import os
+import time
 from BeautifulSoup import BeautifulSoup
+from pync import Notifier
 import sys 
 reload(sys) 
 sys.setdefaultencoding('utf8') 
@@ -17,14 +19,16 @@ def check_tickets(url):
     soup = BeautifulSoup(data)
     img = soup.findAll('img')
     if len(img)>0:
-        concert_title = get_ticket_title(url)
+        concert_title = get_title_by_ticket_url(url)
         print concert_title
         print 'tickets!!!'
-        os.system('echo %s | mutt jollychang@gmail.com -s "you can buy tickets now %s "' % (url, concert_title))
+        Notifier.notify(concert_title, open=url.strip(), title='chncpa ticket is open')
+        time.sleep(1)
+        #os.system('echo %s | mutt jollychang@gmail.com -s "you can buy tickets now %s "' % (url, concert_title))
     else:
         print 'please wait'
 
-def get_ticket_title(url):
+def get_title_by_ticket_url(url):
     concert_detail_data = urllib2.urlopen(url)
     concert_detail_soup = BeautifulSoup(concert_detail_data)
     concert_title = concert_detail_soup.head.title.string
@@ -38,7 +42,8 @@ def get_id_by_ticket_url(url):
 
 
 if __name__=='__main__':
-    file = open('tickets.txt')
+    ticket_list_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tickets.txt')
+    file = open(ticket_list_file)
     for url in file.readlines():
         check_tickets(url)
     file.close()
