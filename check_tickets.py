69e6ac6587415bdad6c86ff60dd3e536df2b120b
jollychang <jollychang@gmail.com>
2018-07-13 12:21:52 +0800

    remove get unit_url

--- check_tickets.py
+++ check_tickets.py
@@ -6,23 +6,19 @@ import os
 import time
 from BeautifulSoup import BeautifulSoup
 from pync import Notifier
-import sys 
-reload(sys) 
-sys.setdefaultencoding('utf8') 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 def check_tickets(url):
     #url = 'http://www.chncpa.org/ycgp/jmxx/2011-03-30/87483.shtml'
     print url
-    id = get_id_by_ticket_url(url)
-    unit_url = 'http://ticket.chncpa.org/unit.jsp?productId=%s' % id
+    keyword = "【开票】"
     headers = {}
     headers['Cookie'] = 'coBUDrHx6D=MDAwM2IyNzNhNDQwMDAwMDAwMDQwZi1MIlYxMzkxNDYzOTYx; Hm_lvt_c0f83d11c5318938a003c0a00dcded64=1391681917; Hm_lpvt_c0f83d11c5318938a003c0a00dcded64=1391681917; _gscu_1063692112=91681917r202yo21; _gscs_1063692112=91681917yri46l21|pv:1; _gscbrs_1063692112=1; JSESSIONID=84yvSzhLyG52DK8JTDdr7hhJrX5nn3L18qp638t916FLmxBTYWs1!-194699159'
-    r = requests.get(unit_url, headers = headers)
-    data =  r.text
-    soup = BeautifulSoup(data)
-    img = soup.findAll('img')
-    if len(img)>0:
+    r = requests.get(url, headers = headers)
+    if keyword in r.text:
         concert_title = get_title_by_ticket_url(url)
         print concert_title
         print 'tickets!!!'
@@ -37,12 +33,6 @@ def get_title_by_ticket_url(url):
     concert_detail_soup = BeautifulSoup(concert_detail_data)
     concert_title = concert_detail_soup.head.title.string
     return concert_title
-
-def get_id_by_ticket_url(url):
-    tmp = url.split('/')
-    tmp = tmp[-1:][0]
-    id = tmp.split('.')[0]
-    return id
 
 
 if __name__=='__main__':
